14d6bdba2b63c943b97c1fb84c4a5ff38723ac6e
Malachi Elkins <melkins1@uab.edu>
2024-09-25 19:14:49 -0500

    Part 1 Files

--- animal.h
+++ animal.h
@@ -1,4 +1,59 @@
 #ifndef _animal_h
 #define _animal_h
 
+#include <string>
+
+class Animal {
+
+ public:
+  //constructor
+  Animal(std::string cName, int cAge, std::string cType);
+
+  //prints animal's details
+  virtual void displayInfo();
+
+  //returns animal's type
+  std::string getType();
+
+ protected:
+
+  std::string name;
+  int age;
+  std::string type;
+
+};
+
+class Dog : public Animal {
+
+ public:
+
+  //constructor (no need for type as a parameter)
+  Dog(std::string cName, int cAge, std::string cBreed);
+
+  //additionally includes breed information
+  void displayInfo() override;
+
+ private:
+
+  std::string breed;
+
+};
+
+class Cat : public Animal {
+
+ public:
+
+  //constructor (no need for type as a parameter)
+  Cat(std::string cName, int cAge, std::string cBreed);
+
+  //additionally includes breed information
+  void displayInfo() override;
+
+ private:
+
+  std::string breed;
+
+};
+  
+  
 #endif
